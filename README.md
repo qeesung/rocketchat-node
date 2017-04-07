6e991f43570f1d0b6eedcf8073f3a8e878f7fc4d
Matthias Kainer <mail@matthias-kainer.de>
2017-04-07 03:21:24 +0200

    Added a way to add the version to the base url (#9)
    * Added tests to add the api version to the makeUri result
    
    * implemented the appending of the version
    
    * Adding example tests for way to update to new version with testable code
    
    * Added the code
    
    * formatted file and added tests for setTopic
    
    * formatted file and added setTopic function
    
    * ignoring vscode settings
    
    * More explicit fork so I can npm install the corrected image
    
    * Added help text about version distinction

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # JavaScript RocketChat API for node.js
 
+**Node this is a fork of [qeesung/rocketchat-node](https://github.com/qeesung/rocketchat-node) and supposed to be rejoined as soon as https://github.com/qeesung/rocketchat-node/pull/9 is closed. Please do not rely on this npm package.**
+
 A node.js module, which provides an object oriented wrapper for the RocketChat REST API.
 
 RocketChat official website address can be found [here](https://rocket.chat/)  .
@@ -21,14 +23,14 @@ This Lib library package the following functions:
 Install with the node package manager [npm](http://npmjs.org/):
 
 ```
-$ npm install rocketchat
+$ npm install rocketchat-node
 ```
 
 or
 
 Install via git clone:
 ```
-$ git clone https://github.com/qeesung/rocketchat-node.git
+$ git clone https://github.com/MatthiasKainer/rocketchat-node.git
 $ cd rocketchat-node
 $ npm install
 ```
@@ -39,7 +41,10 @@ $ npm install
 
 ```
 var RocketChatApi = require('rocketchat').RocketChatApi;
+// alpha-api versions
 var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
+// v1-api versions
+var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password, "v1");
 ```
 
 ### Obtaining the running rocket-chat version
--- test/rocket.test.js
+++ test/rocket.test.js
@@ -10,13 +10,13 @@ var RocketChatApi = require('../lib/rocket-chat').RocketChatApi;
 var should = require("should");
 
 var config = {
-    host:"192.168.1.102", // this is my personal rocketchat server hosted in my laptop
-    port:"3000",
-    user:"qeesung",
-    password:"123456"
+    host: "192.168.1.102", // this is my personal rocketchat server hosted in my laptop
+    port: "3000",
+    user: "qeesung",
+    password: "123456"
 };
 
-describe("Test the rest api and rocketchat version version",function () {
+describe("Test the rest api and rocketchat version version", function () {
     it("rest api version should not be below 0.1 and rocketchat should not be beblow 0.5", function (done) {
         var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
         rocketChatApi.version(function (err, body) {
@@ -29,8 +29,180 @@ describe("Test the rest api and rocketchat version version",function () {
     });
 });
 
+describe("[multiple-api-versions] test multiple api versions", function () {
+    it("should not add a version for alpha versions of gitlab", function (done) {
+        var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
+        rocketChatApi.makeUri("url").should.be.equal("http://192.168.1.102:3000/api/url");
+        done();
+    });
 
-describe("test login the logout",function () {
+    it("should add a version to the base uri for alpha versions of gitlab", function (done) {
+        var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password, "v1");
+        rocketChatApi.makeUri("url").should.be.equal("http://192.168.1.102:3000/api/v1/url");
+        done();
+    });
+});
+
+describe("[versionRequestData] test single versions", function () {
+    describe("nonexisting version", function () {
+        var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password, "please-never-exist-as-version");
+
+        it("should fail", function (done) {
+            var fail;
+            try {
+                rocketChatApi.getRequestData("sendMsg");
+            } catch (err) {
+                err = fail;
+            }
+
+            should(fail).not.be.null;
+            done();
+        });
+    })
+
+    describe("nonexisting function", function () {
+        var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
+
+        it("should fail", function (done) {
+            var fail;
+            try {
+                rocketChatApi.getRequestData("please-never-exist-as-function");
+            } catch (err) {
+                err = fail;
+            }
+
+            should(fail).not.be.null;
+            done();
+        });
+    })
+
+    describe("sendMsg", function () {
+        describe("alpha version", function () {
+            var requestData;
+
+            before(function () {
+                var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
+                requestData = rocketChatApi.getRequestData("sendMsg");
+            });
+
+            it("should return the correct path for this version ", function (done) {
+                var roomId = "roomId";
+                should(requestData).not.be.null;
+                requestData.path({ roomId: roomId }).should.be.equal('rooms/' + roomId + "/send");
+                done();
+            });
+        });
+
+        describe("version1", function () {
+            var requestData;
+
+            before(function () {
+                var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password, "v1");
+                requestData = rocketChatApi.getRequestData("sendMsg");
+            });
+
+            it("should return the correct path for this version ", function (done) {
+                var roomId = "roomId";
+                var message = "string";
+                var data = { roomId: roomId, message: message };
+                should(requestData).not.be.null;
+                requestData.path(data).should.be.equal('chat.postMessage');
+                requestData.body(data).should.be.eql({
+                    "roomId": roomId,
+                    "text": message
+                });
+                done();
+            });
+        })
+    });
+
+    describe("setTopic", function () {
+        var requestData, rocketChatApi;
+
+        describe("alpha version", function () {
+
+            before(function () {
+                rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
+            });
+
+            it("should throw an exception", function (done) {
+                var error = null;
+                try {
+                    requestData = rocketChatApi.getRequestData("setTopic");
+                } catch (err) {
+                    error = err;
+                }
+
+                should(error).not.be.null;
+                error.message.should.be.equal("Method not supported in this version");
+                done();
+            });
+        });
+
+        describe("version1", function () {
+            var requestData;
+
+            before(function () {
+                var rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password, "v1");
+                requestData = rocketChatApi.getRequestData("setTopic");
+            });
+
+            it("should fail on missing data ", function (done) {
+                var error = null;
+                try {
+                    requestData.body(null);
+                } catch (err) {
+                    error = err;
+                }
+                should(error).not.null;
+                error.message.should.be.equal("data cannot be missing");
+                done();
+            });
+
+            it("should fail on missing roomId ", function (done) {
+                var data = { roomId: "roomId" };
+                var error = null;
+                try {
+                    requestData.body(data);
+                } catch (err) {
+                    error = err;
+                }
+                should(error).not.null;
+                error.message.should.be.equal("topic cannot be missing");
+                done();
+            });
+
+            it("should fail on missing topic ", function (done) {
+                var data = { topic: "topic" };
+                var error = null;
+                try {
+                    requestData.body(data);
+                } catch (err) {
+                    error = err;
+                }
+
+                should(error).not.be.null;
+                error.message.should.be.equal("roomId cannot be missing");
+                done();
+            });
+
+            it("should return the correct path for this version ", function (done) {
+                var roomId = "roomId";
+                var topic = "topic";
+                var data = { roomId: roomId, topic: topic };
+                should(requestData).not.be.null;
+                requestData.path(data).should.be.equal('channels.setTopic');
+                requestData.body(data).should.be.eql({
+                    "roomId": roomId,
+                    "topic": topic
+                });
+                done();
+            });
+        })
+    })
+})
+
+describe("test login the logout", function () {
     var rocketChatApi = null;
     beforeEach(function () {
         rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
@@ -81,17 +253,17 @@ describe("test login the logout",function () {
 describe("test create, join, leave rooms, and get list of public rooms", function () {
     var rocketChatApi = null;
     var testConfig = {
-        host:"192.168.1.102", // this is my personal rocketchat server hosted in my laptop
-        port:"3000",
-        user:"testuser",
-        password:"123456"
+        host: "192.168.1.102", // this is my personal rocketchat server hosted in my laptop
+        port: "3000",
+        user: "testuser",
+        password: "123456"
     };
     beforeEach(function () {
         rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
     });
 
     it("create room status should be success", function (done) {
-        var roomName = "testRoom_"+Date.now();// create a room has unique name
+        var roomName = "testRoom_" + Date.now();// create a room has unique name
         rocketChatApi.createRoom(roomName, function (err, body) {
             (!err).should.be.ok();
             body.success.should.be.ok();
@@ -102,7 +274,7 @@ describe("test create, join, leave rooms, and get list of public rooms", functio
 
     it("create a new room with a test user, and join it", function (done) {
         var testUserClient = new RocketChatApi('http', testConfig.host, testConfig.port, testConfig.user, testConfig.password);
-        var roomName = "testuser_testRoom_"+Date.now();// create a room has unique name
+        var roomName = "testuser_testRoom_" + Date.now();// create a room has unique name
         testUserClient.createRoom(roomName, function (err, body) {
             (!err).should.be.ok();
             var roomId = body.channel._id;
@@ -119,7 +291,7 @@ describe("test create, join, leave rooms, and get list of public rooms", functio
 
 
         var testUserClient = new RocketChatApi('http', testConfig.host, testConfig.port, testConfig.user, testConfig.password);
-        var roomName = "testuser_testRoom_"+Date.now();// create a room has unique name
+        var roomName = "testuser_testRoom_" + Date.now();// create a room has unique name
         testUserClient.createRoom(roomName, function (err, body) {
             (!err).should.be.ok();
             var roomId = body.channel._id;
@@ -139,7 +311,7 @@ describe("test create, join, leave rooms, and get list of public rooms", functio
     it("create a new room with a uniqe name, then get the all public rooms to check", function (done) {
 
         var createdRoomId = 0;
-        var roomName = "createdRoom_"+Date.now();// create a room has unique name
+        var roomName = "createdRoom_" + Date.now();// create a room has unique name
         rocketChatApi.createRoom(roomName, function (err, body) {
             (!err).should.be.ok();
             var roomId = body.channel._id;
@@ -160,13 +332,13 @@ describe("test create, join, leave rooms, and get list of public rooms", functio
 
 
 describe("test sending a message and get all messages in a room", function () {
-    var rocketChatApi= null;
+    var rocketChatApi = null;
     beforeEach(function () {
         rocketChatApi = new RocketChatApi('http', config.host, config.port, config.user, config.password);
     });
 
     it("sending a meesage", function (done) {
-        var roomName = "createdRoom_"+Date.now();// create a room has unique name
+        var roomName = "createdRoom_" + Date.now();// create a room has unique name
         var message = "Hello World";
         rocketChatApi.createRoom(roomName, function (err, body) {
             (!err).should.be.ok();
@@ -183,7 +355,7 @@ describe("test sending a message and get all messages in a room", function () {
     });
 
     it("sending a meesage, and get lastest messages", function (done) {
-        var roomName = "createdRoom_"+Date.now();// create a room has unique name
+        var roomName = "createdRoom_" + Date.now();// create a room has unique name
         var message = "Hello World";
         rocketChatApi.createRoom(roomName, function (err, body) {
             (!err).should.be.ok();
