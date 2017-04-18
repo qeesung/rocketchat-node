c729f10141711c03244603e899a6a3d15a14a1c6
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-18 20:32:52 +0200

    Added paging for channels

---
+++ test/channels.test.js
@@ -0,0 +1,64 @@
+var RocketChatClient = require('../lib/rocketChat').RocketChatClient
+var should = require("should");
+var async = require("async");
+
+var config = {
+    host: "127.0.0.1",
+    port: "3000",
+    user: "qeesung",
+    password: "123456"
+};
+
+describe("channels", function () {
+
+    var rocketChatClient = null;
+    before(function (done) {
+        rocketChatClient = new RocketChatClient("http",
+            config.host,
+            config.port,
+            config.user,
+            config.password,
+            done)
+    });
+
+    describe("creating channels", function () {
+        var creationResult = null;
+
+        it("should be successful", function (done) {
+            this.timeout(10000);
+            var creates = [];
+            for(var i=0;i<10;i++) {
+                creates.push(function (callback) {
+                    rocketChatClient.channels.create("channel-name-" + Date.now(), function (err, body) {
+                        should(err).be.null;
+                        should(body.success).be.true;
+                        callback();
+                    })
+                });
+            }
+            async.series(creates, function() {
+                done();
+            });
+        });
+
+        describe("querying channels", function () {
+            it("should be listable", function (done) {
+                rocketChatClient.channels.list(0, 5, function (err, result) {
+                    should(err).be.null;
+                    should(result.success).be.true;
+                    should(result.channels.length).be.equal(5);
+                    done();
+                })
+            })
+            it("should be pageable", function (done) {
+                rocketChatClient.channels.list(1, 5, function (err, result) {
+                    should(err).be.null;
+                    should(result.success).be.true;
+                    should(result.channels.length).be.equal(5);
+                    done();
+                })
+            })
+        })
+    });
+
+})
