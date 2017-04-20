321aff6ba8ed739b6c98f248e9f184eca74a8bc1
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-20 19:15:38 +0200

    a first rough sketch on how subscription might look like as test

---
+++ test/notifyUser.test.js
@@ -0,0 +1,45 @@
+var RocketChatClient = require('../lib/rocketChat').RocketChatClient
+var should = require("should");
+var async = require("async");
+
+var config = {
+    host: "127.0.0.1",
+    port: "3000",
+    user: "qeesung",
+    password: "123456"
+};
+
+describe("notifyUser", function () {
+    let userId;
+
+    before(function (done) {
+        let client = new RocketChatClient("http", config.host, config.port, config.user, config.password, function(err, body) {
+            should(err).be.null();
+            userId = body.userId;
+        });
+    });
+
+    describe("of a new message", function () {
+        let roomId;
+
+        before(function (done) {
+            client.channel.create("notify-user-" + Date.now(), function (err, body) {
+                should(err).be.null();
+                should(body.success).be.true();
+                done();
+            });
+        });
+
+        it("should notify the user when a message for him was sent", function (done) {
+            let message = `@${config.user} hello world!`;
+            client.subscriptions.forUser.onMessage(userId, function(err, body) {
+                should(err).be.null();
+                should(body).not.be.null();
+                should(body.fields.args[0].text).be.equal(message);
+                done();
+            }); 
+
+            client.chat.postMessage({ roomId, text: message });
+        });
+    });
+});
