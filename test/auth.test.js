54ede5aefcf28ae30fb31e7d8700e6c28fd5f5b7
qeesung <1245712564@qq.com>
2017-04-26 00:43:55 +0800

    Clean the auth.test.js file

--- test/auth.test.js
+++ test/auth.test.js
@@ -2,10 +2,10 @@
  * Created by qeesung on 2017/4/16.
  */
 
-var RocketChatClient = require('../lib/rocketChat').RocketChatClient
-var should = require("should");
+const RocketChatClient = require("../lib/rocketChat").RocketChatClient;
+const should = require("should");
 
-var config = {
+const config = {
     host: "127.0.0.1", // this is my personal rocketchat server hosted in my laptop
     port: "3000",
     user: "qeesung",
@@ -13,42 +13,42 @@ var config = {
 };
 
 describe("test login and the logout", function () {
-    var rocketChatClient = null;
+    let rocketChatClient = null;
     beforeEach(function (done) {
         rocketChatClient = new RocketChatClient("http",
             config.host,
             config.port,
             config.user,
             config.password,
-            done)
-    })
+            done);
+    });
     it("logout status should be success and the token should be null", function (done) {
-        should(rocketChatClient.restClient.getHeader("X-Auth-Token")).not.be.null()
-        should(rocketChatClient.restClient.getHeader("X-User-Id")).not.be.null()
-        rocketChatClient.authentication.logout(function (err, result) {
-            should(err).be.null()
-            should(rocketChatClient.restClient.getHeader("X-Auth-Token")).not.be.ok()
-            should(rocketChatClient.restClient.getHeader("X-User-Id")).not.be.ok()
-            done()
-        })
-    })
-})
+        should(rocketChatClient.restClient.getHeader("X-Auth-Token")).not.be.null();
+        should(rocketChatClient.restClient.getHeader("X-User-Id")).not.be.null();
+        rocketChatClient.authentication.logout(function (err) {
+            should(err).be.null();
+            should(rocketChatClient.restClient.getHeader("X-Auth-Token")).not.be.ok();
+            should(rocketChatClient.restClient.getHeader("X-User-Id")).not.be.ok();
+            done();
+        });
+    });
+});
 
 describe("test 'me' interface to get user detail information", function () {
-    var rocketChatClient = null;
+    let rocketChatClient = null;
     beforeEach(function (done) {
         rocketChatClient = new RocketChatClient("http",
             config.host,
             config.port,
             config.user,
             config.password,
-            done)
-    })
+            done);
+    });
     it("user name should equal to "+config.user, function (done) {
         rocketChatClient.authentication.me(function (err, body) {
             should(err).be.null();
-            should.equal(body.username, config.user)
-            done()
-        })
-    })
-})
+            should.equal(body.username, config.user);
+            done();
+        });
+    });
+});
