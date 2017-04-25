7a604918124c2176b0e17a914c34bd68742121e2
Kainer, Matthias <mail@matthias-kainer.de>
2017-04-25 12:44:41 +0200

    Adding co boilerplate

--- test/chat.test.js
+++ test/chat.test.js
@@ -19,34 +19,44 @@ describe("chat", function () {
             config.port,
             config.user,
             config.password, () => {
-                let currentRoom = yield rocketChatClient.channels.create("chat-name-" + Date.now());
-                roomId = currentRoom.channel._id;
-                done();
+                return co(function* () {
+                    let currentRoom = yield rocketChatClient.channels.create("chat-name-" + Date.now());
+                    roomId = currentRoom.channel._id;
+                    done();
+                }).catch((err) => {
+                    should(err).be.null();
+                })
             });
     });
 
     describe("remove", function (done) {
         it("should be able to remove a posted message", function () {
-            let message = yield this.rocketChatClient.chat.postMessage({ roomId, text: "any message" });
-            let msgId = message.message._id;
-            let result = yield.rocketChatClient.chat.remove({ roomId, msgId });
-            should(result).not.be.null();
-            should(result.success).be.true();
-            done();
+            return co(function* () {
+                let message = yield this.rocketChatClient.chat.postMessage({ roomId, text: "any message" });
+                let msgId = message.message._id;
+                let result = yield rocketChatClient.chat.remove({ roomId, msgId });
+                should(result).not.be.null();
+                should(result.success).be.true();
+            }).catch((err) => {
+                should(err).be.null();
+            });
         });
 
         it("should be able to update a posted message", function (done) {
             const updatedText = "updated";
-            let message = yield this.rocketChatClient.chat.postMessage({ roomId, text: "any message" });
-            let msgId = message.message._id;
-            let result = yield.rocketChatClient.chat.update({ roomId, msgId, text: updatedText });
-            should(result).not.be.null();
-            should(result.success).be.true();
-            should(result.message).not.be.null();
-            should(result.message.msg).be.equal(updatedText);
-            should(result.message.editedBy).not.be.null();
-            should(result.message.editedBy.username).be.equal(config.user);
-            done();
+            return co(function* () {
+                let message = yield this.rocketChatClient.chat.postMessage({ roomId, text: "any message" });
+                let msgId = message.message._id;
+                let result = yield rocketChatClient.chat.update({ roomId, msgId, text: updatedText });
+                should(result).not.be.null();
+                should(result.success).be.true();
+                should(result.message).not.be.null();
+                should(result.message.msg).be.equal(updatedText);
+                should(result.message.editedBy).not.be.null();
+                should(result.message.editedBy.username).be.equal(config.user);
+            }).catch((err) => {
+                should(err).be.null();
+            });
         });
     });
 });
