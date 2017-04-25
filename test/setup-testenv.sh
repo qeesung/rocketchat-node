56bd9acb9322ce8e59489d0c821bdcd7ebbde7d1
秦世成 <qeesung@live.com>
2017-04-25 23:57:07 +0800

    lock rocket chat docker image version (#42)
    * lock rocket chat docker image version
    
    * lock the docker image version to 0.54

--- test/setup-testenv.sh
+++ test/setup-testenv.sh
@@ -1,5 +1,5 @@
 docker run --name db -d mongo:3.0 --smallfiles
-docker run -p 3000:3000 --env ROOT_URL=http://127.0.0.1 --env ADMIN_PASS=123456 --env ADMIN_USERNAME=qeesung --name rocketchat --link db -d rocket.chat
+docker run -p 3000:3000 --env ROOT_URL=http://127.0.0.1 --env ADMIN_PASS=123456 --env ADMIN_USERNAME=qeesung --name rocketchat --link db -d rocket.chat:0.54
 
 declare -i max=0
 until [ $(curl --output /dev/null --silent --head --fail http://localhost:3000/api/v1/info) ] || [ "$max" -gt 30 ]; do
